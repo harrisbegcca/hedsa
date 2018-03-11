6563eb7f018b1b40bfc3186127a0fbffcf96dccf
Harris Beg <legostarwarsguy02@gmail.com>
2018-03-11 10:31:28 -0700

    initial files added

---
+++ js/init.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  $('.button-collapse').sideNav();
+  Materialize.showStaggeredList('.introduction');
+  $('.parallax').parallax();
+  $('.carousel.carousel-slider').carousel({fullWidth: true});
+$('#terms').modal();
+});
